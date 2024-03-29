3a85fd9dafba665d2aceb5560ca35ecc506035b9
Enzo <enzowederma@gmail.com>
2024-03-29 17:37:31 -0300

    ✨ feat: add IDUFU syncing

--- src/app/components/start/index.tsx
+++ src/app/components/start/index.tsx
@@ -1,20 +1,28 @@
 'use client'
 
-import { Button, Input, Card, CardHeader, CardBody, Spacer } from "@nextui-org/react"
+import { Button, Card, CardHeader, CardBody, Spacer } from "@nextui-org/react"
 import NextImage from "next/image";
-import ImageIcon from "./image.png";
+import ImageIcon from "../../assets/image.svg";
+import UFULogo from "../../assets/ufu.png";
 import { useState } from "react";
 import { scanQRInput } from "@/app/helper/qrcode";
 
 export const Start = () => {
-    const [data, setData] = useState<File>();
+    const [student, setStudent] = useState<any>({ foto: ImageIcon, default: true });
 
     return (
         <Card className="p-10">
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-center">
-                <p className="text-tiny uppercase font-bold">Controle de Frequência - UFU</p>
+                <NextImage 
+                    width={50}
+                    height={50}
+                    alt="UFU's Logo"
+                    src={UFULogo}
+                />
+                <Spacer y={2}/>
+                <p className="text-tiny uppercase font-bold">Controle de Frequência</p>
             </CardHeader>
-            <Spacer y={10} />
+            <Spacer y={5} />
             <CardBody className="overflow-visible py-2 items-between">
                 <div className="items-center flex flex-col">
                     <label htmlFor="file-input" className="p-12 border-8 rounded-lg border-gray-200">
@@ -22,20 +30,26 @@ export const Start = () => {
                             width={150}
                             height={150}
                             alt="Image Icon"
-                            src={ImageIcon}
-                            className="opacity-25"
+                            src={student.foto}
+                            className={student.default ? "opacity-25" : ""}
                         />
                     </label>
-                    <Input
+                    <Spacer y={5} />
+                    { student.error && <p>{student.error}</p> }
+                    { student.nome && <p>{student.nome}</p> }
+                    <input
                         type="file"
                         accept="image/*"
-                        onChange={({target}) => { if(target.files) scanQRInput(target.files[0]);}}
+                        onChange={({ target }) => { if (target.files && target.files[0]) scanQRInput(target.files[0], setStudent) }}
                         id="file-input"
                         className="hidden"
                     />
+
                 </div>
-                <Spacer y={20} />
                 <div className="items-center flex flex-col">
+                    <Spacer y={10} />
+                    <Button>Confirmar</Button>
+                    <Spacer y={5} />
                     <Button>Cadastrar-se</Button>
                 </div>
             </CardBody>
--- src/app/helper/qrcode.ts
+++ src/app/helper/qrcode.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
 import QrScanner from 'qr-scanner';
+import ImageIcon from "../assets/image.svg";
+import { Dispatch, SetStateAction } from 'react';
 
-export const scanQRInput = async (qrInput: File) => {
+export const scanQRInput = async (qrInput: File, fn: Dispatch<SetStateAction<any>>) => {
   QrScanner.scanImage(qrInput, { returnDetailedScanResult: true })
     .then(result => result.data.slice(-14))
     .then(result =>
       axios.get(`https://www.sistemas.ufu.br/valida-gateway/id-digital/buscarDadosIdDigital?idIdentidade=${result}`)
         .then(response => response.data.identidadeDigital)
-        .then((response) => {return response})
-        .catch(e => alert('Não conseguimos encontrar um IDUFU válido na ultima imagem.'))
+        .then((response) => fn({...response, foto: "data:image/png;base64," + response.foto}))
     )
-    .catch(e => alert('Não conseguimos encontrar um QRCode válido na ultima imagem.'));
+    .catch(e => fn({foto: ImageIcon, default: true, error: "QRCode Inválido" }));
 };
